a4ce73b2ec607f741ab5750421c41a9a74d3ecf9
aaronpoweruser <atlrudeboi1@gmail.com>
2013-01-16 18:43:08 -0800

    Enable gcc 4.7 and O3
    Change-Id: I3777a61f6d84113215576c5be0f39869cdda2db5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -35,12 +35,13 @@ TARGET_CPU_SMP := true
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
-#ARCH_ARM_USE_NON_NEON_MEMCPY := true
-TARGET_USE_LINARO_STRING_ROUTINES :=true
-#TARGET_USE_LINAROs_MEMCPY :=true
- TARGET_EXTRA_CFLAGS += $(call cc-option,-mtune=cortex-a9) $(call cc-option,-mcpu=cortex-a9)
-ifneq (,$(filter true 1,$(TARGET_INCLUDE_EXTRA_CFLAGS)))
-endif
+ARCH_ARM_USE_NON_NEON_MEMCPY := true
+TARGET_USE_LINARO_STRING_ROUTINES := true
+TARGET_GCC_VERSION := 4.7
+TARGET_USE_O3 := true
+#TARGET_USE_LINARO_MEMCPY :=true
+TARGET_EXTRA_CFLAGS += $(call cc-option,-mtune=cortex-a9) $(call cc-option,-mcpu=cortex-a9)
+
 
 # Set to true for platforms with 32 byte L2 cache line.
 ARCH_ARM_HAVE_32_BYTE_CACHE_LINES := true
